9ae4841026c44fd9a5bd3d97c22e45f5d3e5b7d1
Flower2ree <shryu1019@sju.ac.kr>
2024-04-19 14:49:10 +0900

    feat: Rename remove_odd -> split_odd_even
    change return type tuple
    :

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ def check_available():
 def start():
     return "1"
 
+def split_odd(nums=[1,2,3,4]):
+    odds = []
+    evens = []
+    for i in nums:
+        if i%2 == 0 : 
+            evens.append(i)
+        else:
+            odds.append(i)
+
+    return (odds , evens)
+
+
 if __name__=='__main__':
     print(check_available())
-    start()
+    print(split_odd)
